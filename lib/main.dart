ada7035995253fd57b0d9f1cc07a2d88d33ea5e5
ramyhany0 <ramyhany161@gmail.com>
2024-06-25 22:18:11 +0300

    change phone number

---
+++ lib/main.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(const BusinessCardApp());
+}
+
+class BusinessCardApp extends StatelessWidget {
+  const BusinessCardApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: const Color(0xFF2a465e),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const CircleAvatar(
+              radius: 102,
+              backgroundColor: Colors.white,
+              child: CircleAvatar(
+                radius: 100,
+                backgroundImage:
+                    AssetImage("images/IMG_20200719_183552_1-01.jpeg"),
+              ),
+            ),
+            const Text(
+              "Ramy Hany",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 32,
+                fontFamily: "Pacifico",
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 5),
+              child: Text(
+                "FLUTTER DEVELOPER",
+                style: TextStyle(
+                  color: Color(0xFF6C8090),
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const Divider(
+              color: Color(0xFF6C8090),
+              thickness: 1,
+              indent: 50,
+              endIndent: 50,
+              height: 5,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: Colors.white,
+                ),
+                height: 65,
+                child: Row(
+                  children: const [
+                    Padding(
+                      padding: EdgeInsets.only(left: 16),
+                      child: Icon(
+                        Icons.phone,
+                        size: 32,
+                        color: Color(0xFF2a465e),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 22),
+                      child: Text(
+                        "(+20) **********",
+                        style: TextStyle(fontSize: 24),
+                      ),
+                    ),
+                    Spacer(
+                      flex: 3,
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: Colors.white,
+                ),
+                height: 65,
+                child: Row(
+                  children: const [
+                    Padding(
+                      padding: EdgeInsets.only(left: 16),
+                      child: Icon(
+                        Icons.mail,
+                        size: 32,
+                        color: Color(0xFF2a465e),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 22),
+                      child: Text(
+                        "ramyhany161@gmail.com",
+                        style: TextStyle(fontSize: 24),
+                      ),
+                    ),
+                    Spacer(
+                      flex: 3,
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
